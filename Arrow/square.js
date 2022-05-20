fd59e187881fa6418cd70722a66d17927f90dd52
varsha-504 <antargangivarsha.com>
2022-05-20 22:39:09 +0530

    added programs on Arrow function

---
+++ Arrow/square.js
@@ -0,0 +1,11 @@
+// const sqr = function (num){
+//     return num*num;
+// }
+const sqr = (num) => num*num
+console.log(sqr(6));
+
+// const cube = function (n1){
+//     return n1*n1;
+// }
+const cube = (n1) => n1*n1*n1
+console.log(cube(6));
